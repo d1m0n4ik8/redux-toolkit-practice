879b514f3238109d4ce38e804041f54e9ff8a407
d1m0n4ik <svyryd2012@gmail.com>
2023-06-20 20:38:28 +0300

    react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import Counter from './Components/Counter'
+import { Suspense, lazy } from 'react'
+
+import Footer from './Components/Footer'
+import Header from './Components/Header'
+import { Route, Routes } from 'react-router-dom'
+
+const Counter = lazy(() => import('./Components/Counter'))
 
 const App = () => {
    return (
-      <div className="w-screen h-screen">
-         <Counter />
-      </div>
+      <Suspense fallback={<>loading...</>}>
+         <div className="w-screen h-screen flex flex-col ">
+            <Header />
+            <main className="container flex-grow mx-auto">
+               <Routes>
+                  <Route path="/" element={<Counter />} />
+                  <Route path="/counter" element={<Counter />} />
+                  <Route path="/*" element={<>not found</>} />
+               </Routes>
+            </main>
+            <Footer />
+         </div>
+      </Suspense>
    )
 }
 export default App
